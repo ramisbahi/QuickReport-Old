7d33901220c6f6da833fbf50dd263983b126b110
Rami Sbahi <rami.sbahi@duke.edu>
2019-11-02 18:49:33 -0400

    changed name to training data

---
+++ ImageClassifierPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+Invokes Create ML to train images of flowers.
+*/
+
+import CreateMLUI
+
+let trainingClassifier = MLImageClassifierBuilder()
+
+trainingClassifier.showInLiveView()
